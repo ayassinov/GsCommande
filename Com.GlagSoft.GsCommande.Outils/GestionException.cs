e3d4b51ada3cba7693e0825b8493117dc9cd0455
Abdrabba Yassine <yassine2004@gmail.com>
2011-06-27 18:19:00 +0100

    adding log

--- Com.GlagSoft.GsCommande.Outils/GestionException.cs
+++ Com.GlagSoft.GsCommande.Outils/GestionException.cs
@@ -1,23 +1,28 @@
 using System;
 using System.Windows.Forms;
+using NLog;
 
 namespace Com.GlagSoft.GsCommande.Outils
 {
     public static class GestionException
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public static void TraiterException(Exception exception, string title)
         {
+            Logger.LogException(LogLevel.Error, exception.Message, exception);
             MessageBox.Show("Une erreur inattendu s'est produite pendant l'exécution du programme", title, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        public static void TraiterException(Exception exception, string  msg, string title)
+        public static void TraiterException(Exception exception, string msg, string title)
         {
+            Logger.LogException(LogLevel.Error, exception.Message, exception);
             MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         public static void LogOnly(Exception exception)
         {
-            //todo log Exception
+            Logger.LogException(LogLevel.Error, exception.Message, exception);
         }
     }
 }
